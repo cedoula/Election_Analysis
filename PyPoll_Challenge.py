9e51c3c991831d61d16d263dc7510cc499962d49
cedoula <cedric.lutonda@gmail.com>
2020-08-11 15:57:31 -0500

    Updated Pypoll_Challenge code to save county results to txt file

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -101,20 +101,21 @@ with open(file_to_save, "w") as txt_file:
          # 6d: Print the county results to the terminal.
         print(f"{county}: {county_percentage:.1f}% ({county_votes:,})")
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(f"{county}: {county_percentage:.1f}% ({county_votes:,})\n")
          # 6f: Write a decision statement to determine the winning county and get its vote count.
         if county_votes > highest_turnout:
             highest_turnout_county = county
             highest_turnout = county_votes
 
     # 7: Print the county with the largest turnout to the terminal.
-    print(
+    largest_county_result = (
         f"\n-------------------------\n"
         f"Largest County Turnout: {highest_turnout_county}\n"
         f"-------------------------\n")
+    print(largest_county_result)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_county_result)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
